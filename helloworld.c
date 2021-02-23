bb531ccef508d8f5f6eb502fad69a022cf7cf9c3
hungnv_SET <32673075+hungnguyenbk@users.noreply.github.com>
2021-02-23 15:51:28 +0700

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,16 @@
 
 int main(){
 	printf ("hello world\n");
+	
+	//SUb string 
+	size_t pos_ = 0;
+	std::string token;
+	while ((pos_ = line.find(delimiter)) != std::string::npos) {
+	token = line.substr(0, pos_);
+	arr.push_back(token);
+	line.erase(0, pos_ + delimiter.length());
+	}
+	arr.push_back(line);
+
 	return 0;
 }
